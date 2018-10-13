90555b8f13a0436610aedbd7f1cdd668b9672eac
Maggie Koesno <maggiekoesno@Maggies-MacBook-Pro.local>
2018-10-14 07:30:28 +0800

    add detection image

---
+++ Object_detection_image.py
@@ -0,0 +1,298 @@
+######## Image Object Detection Using Tensorflow-trained Classifier #########
+#
+# Author: Evan Juras
+# Date: 1/15/18
+# Description: 
+# This program uses a TensorFlow-trained classifier to perform object detection.
+# It loads the classifier uses it to perform object detection on an image.
+# It draws boxes and scores around the objects of interest in the image.
+
+## Some of the code is copied from Google's example at
+## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
+
+## and some is copied from Dat Tran's example at
+## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
+
+## but I changed it to make it more understandable to me.
+
+# Import packages
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import sys
+import tarfile
+from six.moves import urllib
+import re
+import argparse
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+FLAGS = None
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
+class NodeLookup(object):
+    """Converts integer node ID's to human readable labels."""
+
+    def __init__(self,
+                 label_lookup_path=None,
+                 uid_lookup_path=None):
+        if not label_lookup_path:
+            label_lookup_path = os.path.join(
+                FLAGS.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+        if not uid_lookup_path:
+            uid_lookup_path = os.path.join(
+                FLAGS.model_dir, 'imagenet_synset_to_human_label_map.txt')
+        self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+    def load(self, label_lookup_path, uid_lookup_path):
+        """Loads a human readable English name for each softmax node.
+
+        Args:
+          label_lookup_path: string UID to integer node ID.
+          uid_lookup_path: string UID to human-readable string.
+
+        Returns:
+          dict from integer node ID to human-readable string.
+        """
+        if not tf.gfile.Exists(uid_lookup_path):
+            tf.logging.fatal('File does not exist %s', uid_lookup_path)
+        if not tf.gfile.Exists(label_lookup_path):
+            tf.logging.fatal('File does not exist %s', label_lookup_path)
+
+        # Loads mapping from string UID to human-readable string
+        proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+        uid_to_human = {}
+        p = re.compile(r'[n\d]*[ \S,]*')
+        for line in proto_as_ascii_lines:
+            parsed_items = p.findall(line)
+            uid = parsed_items[0]
+            human_string = parsed_items[2]
+            uid_to_human[uid] = human_string
+
+        # Loads mapping from string UID to integer node ID.
+        node_id_to_uid = {}
+        proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+        for line in proto_as_ascii:
+            if line.startswith('  target_class:'):
+                target_class = int(line.split(': ')[1])
+            if line.startswith('  target_class_string:'):
+                target_class_string = line.split(': ')[1]
+                node_id_to_uid[target_class] = target_class_string[1:-2]
+
+        # Loads the final mapping of integer node ID to human-readable string
+        node_id_to_name = {}
+        for key, val in node_id_to_uid.items():
+            if val not in uid_to_human:
+                tf.logging.fatal('Failed to locate: %s', val)
+            name = uid_to_human[val]
+            node_id_to_name[key] = name
+
+        return node_id_to_name
+
+    def id_to_string(self, node_id):
+        if node_id not in self.node_lookup:
+            return ''
+        return self.node_lookup[node_id]
+
+
+def create_graph():
+    """Creates a graph from saved GraphDef file and returns a saver."""
+    # Creates graph from saved graph_def.pb.
+    with tf.gfile.FastGFile(os.path.join(
+            FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_image(image):
+    if not tf.gfile.Exists(image):
+        tf.logging.fatal('File does not exist %s', image)
+    image_data = tf.gfile.FastGFile(image, 'rb').read()
+
+    # Creates graph from saved GraphDef.
+    create_graph()
+
+    with tf.Session() as sess:
+        # Some useful tensors:
+        # 'softmax:0': A tensor containing the normalized prediction across
+        #   1000 labels.
+        # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+        #   float description of the image.
+        # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+        #   encoding of the image.
+        # Runs the softmax tensor by feeding the image_data as input to the graph.
+        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        # Creates node ID --> English string lookup.
+        node_lookup = NodeLookup()
+
+        top_k = predictions.argsort()[-FLAGS.num_top_predictions:][::-1]
+        for node_id in top_k:
+            human_string = node_lookup.id_to_string(node_id)
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+            return human_string
+
+
+def maybe_download_and_extract():
+    """Download and extract model tar file."""
+    dest_directory = FLAGS.model_dir
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+                filename, float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+    tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+# Name of the directory containing the object detection module we're using
+MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
+IMAGE_NAME = 'vid6.jpg'
+
+
+# Grab path to current working directory
+CWD_PATH = os.getcwd()
+
+# Path to frozen detection graph .pb file, which contains the model that is used
+# for object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+# Path to image
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+PATH_TO_IMAGE = os.path.join(PATH_TO_TEST_IMAGES_DIR,IMAGE_NAME)
+
+# Load the label map.
+# Label maps map indices to category names, so that when our convolution
+# network predicts `5`, we know that this corresponds to `king`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+# label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+# categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+# category_index = label_map_util.create_category_index(categories)
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+
+def main(_):
+    maybe_download_and_extract()
+    image = os.path.join(PATH_TO_TEST_IMAGES_DIR, IMAGE_NAME)
+    title_test = run_inference_on_image(image)
+    xx=1
+    for i in category_index:
+        category_index[i] = {'id':xx, 'name':title_test}
+        xx+=1
+    # Load the Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    # Define input and output tensors (i.e. data) for the object detection classifier
+
+    # Input tensor is the image
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Output tensors are the detection boxes, scores, and classes
+    # Each box represents a part of the image where a particular object was detected
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represents level of confidence for each of the objects.
+    # The score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+    # Number of objects detected
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Load image using OpenCV and
+    # expand image dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    image = cv2.imread(PATH_TO_IMAGE)
+    image_expanded = np.expand_dims(image, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.80)
+
+    # All the results have been drawn on image. Now display the image.
+    cv2.imwrite(os.path.join(PATH_TO_TEST_IMAGES_DIR,"Classified_"+IMAGE_NAME), image)
+    cv2.imshow("Result", image)
+
+    # Press any key to close the image
+    cv2.waitKey(0)
+
+    # Clean up
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # classify_image_graph_def.pb:
+    #   Binary representation of the GraphDef protocol buffer.
+    # imagenet_synset_to_human_label_map.txt:
+    #   Map from synset ID to a human readable string.
+    # imagenet_2012_challenge_label_map_proto.pbtxt:
+    #   Text representation of a protocol buffer mapping a label to synset ID.
+    parser.add_argument(
+        '--model_dir',
+        type=str,
+        default='/tmp/imagenet',
+        help="""\
+      Path to classify_image_graph_def.pb,
+      imagenet_synset_to_human_label_map.txt, and
+      imagenet_2012_challenge_label_map_proto.pbtxt.\
+      """
+    )
+    parser.add_argument(
+        '--image_file',
+        type=str,
+        default='',
+        help='Absolute path to image file.'
+    )
+    parser.add_argument(
+        '--num_top_predictions',
+        type=int,
+        default=5,
+        help='Display this many predictions.'
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
